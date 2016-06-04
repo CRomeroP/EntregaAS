85c72c86b91a0ec442fee0f6c46d41f3f5e52026
xavier <xavier.algarra@est.fib.upc.edu>
2016-06-04 19:39:29 +0200

    packages + presentacio

---
+++ Entrega1/src/Presentacio/ReservaVistaController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Presentacio;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author xavier
+ */
+public class ReservaVistaController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
