3a3835ef99071c0fb1561aa3c9de68756ddd5407
Victor Gonzalez <victorggep@gmail.com>
2016-06-07 18:41:55 +0200

    fallo solucionat

--- Entrega1/src/domain/Model/Recurs.java
+++ Entrega1/src/domain/Model/Recurs.java
@@ -1,6 +1,7 @@
 package domain.Model;
 // Generated 14-abr-2016 8:24:58 by Hibernate Tools 4.3.1
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Set;
@@ -29,9 +30,9 @@ public class Recurs  implements java.io.Serializable {
     @Column(name = "nom")
     private String nom;
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "recurs")
-    private Set<ReservaAmbNotificacio> reservasambnotificacio;
+    private ArrayList<ReservaAmbNotificacio> reservasambnotificacio;
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "recurs")
-    private Set<ReservaSenseNotificacio> reservassensenotificacio;
+    private ArrayList<ReservaSenseNotificacio> reservassensenotificacio;
     @Column(name = "type", unique = false, nullable = true, length = 50)
     private String type;     
 
@@ -50,20 +51,20 @@ public class Recurs  implements java.io.Serializable {
         this.nom = nom;
     }
     
-    public Set<ReservaAmbNotificacio> getReservasAmbNotificacio() {
+    public ArrayList<ReservaAmbNotificacio> getReservasAmbNotificacio() {
 	return this.reservasambnotificacio;
     }
 
-    public void setReservasAmbNotificacio(Set<ReservaAmbNotificacio> reservas) {
+    public void setReservasAmbNotificacio(ArrayList<ReservaAmbNotificacio> reservas) {
 	this.reservasambnotificacio = reservas;
     }
 
    
-    public Set<ReservaSenseNotificacio> getReservasSenseNotificacio() {
+    public ArrayList<ReservaSenseNotificacio> getReservasSenseNotificacio() {
         return reservassensenotificacio;
     }
 
-    public void setReservasSenseNotificacio(Set<ReservaSenseNotificacio> reservassensenotificacio) {
+    public void setReservasSenseNotificacio(ArrayList<ReservaSenseNotificacio> reservassensenotificacio) {
         this.reservassensenotificacio = reservassensenotificacio;
     }
 
@@ -81,12 +82,13 @@ public class Recurs  implements java.io.Serializable {
         int i = 0;
         while (i < this.reservasambnotificacio.size() && i < this.reservassensenotificacio.size() && b){
             if (i < this.reservasambnotificacio.size()){
-                this.reservasambnotificacio.get(i)
+                this.reservasambnotificacio.get(i).estaDisponible(d, horai, horaf);
             }
             if (i < reservassensenotificacio.size()){
                 
             }
         }
+        return null;
     }
 }
 
