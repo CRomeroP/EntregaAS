95968d606344d90ee7e6c651ba434517d776acb3
Victor Gonzalez <victorggep@gmail.com>
2016-06-11 12:35:16 +0200

    Merge branch 'master' of https://github.com/CRomeroP/EntregaAS
    # Conflicts:
    #	Entrega1/src/domain/Model/ReservaAmbNotificacio.java
    #	Entrega1/src/test/main2.java

--- Entrega1/src/Presentacio/ReservaVistaController.java
+++ Entrega1/src/Presentacio/ReservaVistaController.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Date;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -19,10 +20,12 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.DateCell;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.Spinner;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 
 /**
  * FXML Controller class
@@ -51,7 +54,25 @@ public class ReservaVistaController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        final Callback<DatePicker, DateCell> dayCellFactory;
+        dayCellFactory = new Callback<DatePicker, DateCell>() {
+            @Override
+            public DateCell call(final DatePicker datePicker) {
+                return new DateCell() {
+                    @Override
+                    public void updateItem(LocalDate item, boolean empty) {
+                        super.updateItem(item, empty);
+                        LocalDate d = LocalDate.now();
+                        if (item.getDayOfYear() < d.getDayOfYear() || item.getYear() < d.getYear()) {
+                            setDisable(true);
+                            setStyle("-fx-background-color: #ffc0cb;");
+                        }
+                       
+                    }
+                };
+            }
+        };
+        this.calendario.setDayCellFactory(dayCellFactory);
     }
     
     @FXML
