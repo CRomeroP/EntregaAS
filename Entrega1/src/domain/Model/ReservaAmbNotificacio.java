3d6ae6411c105d22e5ab41b87e7b1fdfcbe69ac1
Victor Gonzalez <victorggep@gmail.com>
2016-06-12 15:40:54 +0200

    reservaCaducada be

--- Entrega1/src/domain/Model/ReservaAmbNotificacio.java
+++ Entrega1/src/domain/Model/ReservaAmbNotificacio.java
@@ -179,7 +179,7 @@ public class ReservaAmbNotificacio implements Serializable{
     public ArrayList<Usuari> getPossiblesUsuaris(ArrayList<Usuari> u){
         Date fechaActual = new Date();
         //int error = data.compareTo(fechaActual);
-        if ((data.compareTo(fechaActual) < 0) || (data.compareTo(fechaActual) == 0 && horainici > fechaActual.getHours())) throw new ReservaCaducada();
+        if (data.compareTo(fechaActual) == 0 && horainici < fechaActual.getHours()) throw new ReservaCaducada();
         //if ((error == 1) || (error == 0 && horainici > fechaActual.getHours())) throw new ReservaCaducada();
         return getUsuarisSenseNot(u);
     }
